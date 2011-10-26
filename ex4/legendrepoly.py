d4080b56980508a5daeeae86add16b3e99d2967c
RafiKueng <rafi.kueng@gmx.ch>
2011-10-26 13:59:31 +0200

    ex 4 legendrepolz start

---
+++ ex4/legendrepoly.py
@@ -0,0 +1,27 @@
+from derrint import fivepoint
+import numpy as np
+
+
+class LGP:
+	def __init__(self, n, coef):
+		self.n = n
+		self.coef = coef
+	
+	def eval(x=0):
+		n = len(self.coef) - 1
+		sum = c[-1]
+		for k in range(n, 0,-1) :
+			sum = x * sum + c[k-1]
+		return sum
+
+	def prnt():
+		print 'LGP'
+
+def genLegendrePoly(n = 3):
+	
+	
+	return False
+
+ 
+if __name__ == '__main__':
+	genLegendrePoly(5)
